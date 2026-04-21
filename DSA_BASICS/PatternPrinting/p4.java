233a7c3da70625a4d7976dd875e4c1ad5bdf44ef
satakshi003 <soumya3955@gmail.com>
2026-04-21 22:33:58 +0530

    p4 in pattern printing

---
+++ DSA_BASICS/PatternPrinting/p4.java
@@ -0,0 +1,21 @@
+package DSA_BASICS.PatternPrinting;
+
+import java.util.Scanner;
+
+public class p4 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter no. of rows and cols:");
+    int row = sc.nextInt();
+    int col = sc.nextInt();
+
+    for(int i = 1; i<=row; i++){
+      
+      for(int j=1; j<=col; j++){
+        System.out.print( i + " " );
+      }
+      System.out.println();
+    }
+  }
+  
+}
