0827d83d1506392440799125b898b033691ea258
satakshi003 <soumya3955@gmail.com>
2026-04-21 22:34:41 +0530

    p7 in pattern printing

---
+++ DSA_BASICS/PatternPrinting/p7.java
@@ -0,0 +1,20 @@
+package DSA_BASICS.PatternPrinting;
+
+import java.util.Scanner;
+
+public class p7 {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter value of n:");
+    int n = sc.nextInt();
+    
+
+    for(int row = 1; row<=n; row++){
+      
+      for(int col=1; col<=row; col++){
+        System.out.print( col + " " );
+      }
+      System.out.println();
+    }
+  }
+}
