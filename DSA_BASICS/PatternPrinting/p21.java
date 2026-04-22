8815b7c37b890fbd69ace07c55f9471f19731dae
satakshi003 <soumya3955@gmail.com>
2026-04-22 13:54:37 +0530

    p15 in pattern printing

---
+++ DSA_BASICS/PatternPrinting/p21.java
@@ -0,0 +1,29 @@
+package DSA_BASICS.PatternPrinting;
+
+import java.util.Scanner;
+
+public class p21 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for(int i = 1; i<=n; i++){
+      for(int j = n- i; j>=1; j--){
+        System.out.print("   ");
+      }
+      for(int j = 1; j<=(2*i)-1; j++){
+        System.out.print(" * ");
+      }
+      System.out.println();
+    }
+    for(int i = n-1; i>=1; i--){
+      for(int j = n- i; j>=1; j--){
+        System.out.print("   ");
+      }
+      for(int j = 1; j<=(2*i)-1; j++){
+        System.out.print(" * ");
+      }
+      System.out.println();
+    }
+  }
+}
