94d9d47f0251ad71173ed0ec27829733d281b6a1
satakshi003 <soumya3955@gmail.com>
2026-04-24 15:42:35 +0530

    p2 in Arrays

---
+++ Arrays/p2.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class p2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the Array:");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    System.out.println("Enter the array elements: ");
+    for(int i = 0; i<n; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+      
+        sum += arr[i];
+        
+      }
+      System.out.print(sum + " ");
+    }
+  
+  
+}
