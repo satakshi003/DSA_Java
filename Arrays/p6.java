5daeea3380c56ee1f49e9368ab8903b2974dbbd8
satakshi003 <soumya3955@gmail.com>
2026-04-26 13:37:59 +0530

    p6 in java

---
+++ Arrays/p6.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class p6 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the Array:");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    System.out.println("Enter the array elements: ");
+    for(int i = 0; i<n; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    int product = 1;
+    for(int i = 0; i<n; i++){
+      product *= arr[i]; 
+      
+    }
+      
+    System.out.println("Product of the array is:" +  product);
+      
+}
+  
+    }  
+
