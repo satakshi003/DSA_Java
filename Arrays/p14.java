9e5ef0bd7c545fc8a7fce4cf6cb5767a9b8bbf57
satakshi003 <soumya3955@gmail.com>
2026-04-27 17:59:09 +0530

    p14 in Arrays

---
+++ Arrays/p14.java
@@ -0,0 +1,43 @@
+//segragate 0's and 1's (Two pointer approach)
+package Arrays;
+
+import java.util.Scanner;
+
+public class p14 {
+  public static void swap(int[] arr, int i, int j){
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the Array:");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    System.out.println("Enter the array elements: ");
+    for(int i = 0; i<n; i++){
+      arr[i] = sc.nextInt();
+    }
+    
+    int start = 0;
+    int end = n-1;
+    while(start < end){
+      if(arr[start] == 0){
+        start ++;
+      }
+      else if(arr[end] == 1){
+        end--;
+      }
+      else{
+        swap(arr, start, end);
+        start++;
+        end--;
+      }
+    }
+    for(int i=0; i<n; i++){
+      System.out.print(arr[i] + " ");
+    }
+    
+    }  
+}
