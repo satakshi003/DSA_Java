acc4512654d6eb9bdf5e13c269de5a6eb07f17d5
satakshi003 <soumya3955@gmail.com>
2026-05-01 12:28:31 +0530

    Bubble Sort Better approach in Sorting

---
+++ Sorting/BubbleSortBetter.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+public class BubbleSortBetter {
+  public static void print(int[] arr){
+    for(int i = 0; i<arr.length; i++){
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {5, -2, 6, 7, 2,0, 7, 2};
+    int n = arr.length;
+    print(arr);
+
+    for(int i= 0; i<n-1; i++){
+      boolean isSorted = true;
+      for(int j = 0; j<n-1-i; j++){
+        if(arr[j]>arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          isSorted = false;
+        }
+      }
+      if(isSorted == true)
+      break;
+    }
+    print(arr);
+  }
+}
