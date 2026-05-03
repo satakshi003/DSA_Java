4ab0a6ad1495c32d60d04ee34098c53502789331
satakshi003 <soumya3955@gmail.com>
2026-05-03 13:34:27 +0530

    finding peak element in a mountain array using binary search

---
+++ Sorting/BinarySearch/peakInMountainArray.java
@@ -0,0 +1,24 @@
+package Sorting.BinarySearch;
+
+public class peakInMountainArray {
+  public static void main(String[] args) {
+    int[] arr = {2, 4, 6, 8, 10, 8, 5};
+    int n = arr.length;
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end){
+      int mid = start + ((end - start)/2);
+      if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+        System.out.println("Index of peak element is:" + " " + mid);
+        break;
+      }
+      else if(arr[mid]>arr[mid-1]){
+        start = mid+1;
+      }
+      else{
+        end = mid-1;
+      }
+    }
+  }
+}
