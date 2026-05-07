12a79acbc24952f22715b38e1c882435c91a9c6b
satakshi003 <soumya3955@gmail.com>
2026-05-07 13:38:34 +0530

    basics in strings

---
+++ Strings/basics.java
@@ -0,0 +1,20 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class basics {
+  public static void main(String[] args) {
+    /*char[] arr = {'c', 'h', 'e', 'p', 'd', 'u'};
+    int n = arr.length;
+    for(int i=0; i<n; i++ ){
+      System.out.print((char)arr[i] + " ");
+    }*/
+
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+    System.out.println(input);
+
+    String input2 = sc.next();
+    System.out.println(input2);
+  }
+}
