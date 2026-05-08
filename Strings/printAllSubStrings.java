eb90ff73ad4ad657327ef02fa70d14b7eba5a963
satakshi003 <soumya3955@gmail.com>
2026-05-08 16:27:42 +0530

    print all the sub strings in a given string

---
+++ Strings/printAllSubStrings.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class printAllSubStrings {
+  public static void main(String[] args) {
+    String s = "gopi";
+
+    for(int i = 0; i<s.length(); i++){
+      for(int j = i+1; j<=s.length(); j++){
+        System.out.println(s.substring(i, j));
+      }
+    }
+  }
+}
