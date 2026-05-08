006f59da56fa97f8c1b30d051695da7bb0850757
satakshi003 <soumya3955@gmail.com>
2026-05-08 16:27:09 +0530

    subString using built in method

---
+++ Strings/subString.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class subString {
+  public static void main(String[] args) {
+    String s = "AshmitSatakshi";
+    System.out.println(s.substring(0));
+    System.out.println(s.substring(0,6 ));
+     System.out.println(s.substring(6,14
+
+     ));
+  }
+}
