80a7931924e07b1bf1ec9885f6dc0e91de234cc7
satakshi003 <soumya3955@gmail.com>
2026-05-13 18:42:11 +0530

    factorial of a number using recursion

---
+++ Recursion/factorial.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class factorial {
+  public static int fact(int n){
+    if(n == 0 || n == 1){
+      return 1;
+    }
+    return (n * fact(n-1));
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    System.out.println(fact(num));
+  }
+}
