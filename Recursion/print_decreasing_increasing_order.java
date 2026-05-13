06c4980063e7d3141546ed1c5f8ad6a482722122
satakshi003 <soumya3955@gmail.com>
2026-05-13 18:27:26 +0530

    print in decreasing increasing order

---
+++ Recursion/print_decreasing_increasing_order.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class print_decreasing_increasing_order {
+  public static void print(int n){
+    if(n==0) return;
+    System.out.println(n);
+    print(n-1); 
+    if(n!=1) System.out.println(n);
+  }
+
+  public static void main(String[] args) {
+    print(5);
+  }
+}
