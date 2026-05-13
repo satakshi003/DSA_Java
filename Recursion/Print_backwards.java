6e410992242b00875f8963e582c5dd8fa8f7f9c6
satakshi003 <soumya3955@gmail.com>
2026-05-13 12:28:55 +0530

    print n to n using recursion

---
+++ Recursion/Print_backwards.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Print_backwards {
+  public static void print(int n){
+    if(n==0) return;
+    System.out.println(n);
+    print(n-1);
+  }
+
+  public static void main(String[] args) {
+    print(5);
+  }
+}
