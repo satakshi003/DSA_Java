5a5e8cce8a79ad670c6f8af4eebf4aac4bc4459d
satakshi003 <soumya3955@gmail.com>
2026-05-14 19:40:23 +0530

    gcd of two numbers

---
+++ Recursion/GCD_of_2_nums.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class GCD_of_2_nums {
+ /*  public static void main(String[] args) {
+    int num1 = 12;
+    int num2 = 16;
+    int gcd = 0;
+    
+    for(int i = 1; i<Math.min(num1, num2); i++){
+      if(num1%i == 0 && num2%i == 0){
+        gcd = i;
+      }
+    }
+    System.out.println(gcd);
+  }*/
+
+    //GCD using Recursion
+    public static int gcd(int a, int b){
+      if(b==0){
+        return a;
+      } 
+      return (gcd(b, a%b));
+    }
+    public static void main(String[] args) {
+      System.out.println(gcd(12, 16));
+    }
+}
