dffb79a63647d6226b57606e060310455b6621e6
satakshi003 <soumya3955@gmail.com>
2026-05-31 22:45:21 +0530

    reverse queues

--- Queues/reverseQueue.java
+++ Queues/reverseQueue.java
@@ -15,6 +15,20 @@ public class reverseQueue {
     }
   }
 
+  public static void reverseUpToK(Queue<Integer> q, int k){
+    int n = q.size();
+    int[] arr = new int[k+1];
+    for(int i = 0; i<=k; i++){
+      arr[i] = q.remove();
+    }
+    for(int i = arr.length-1; i>=0; i--){
+      q.add(arr[i]);
+    }
+    for(int i = 0; i<n-k-1; i++){
+      q.add(q.remove());
+    }
+  }
+
   
   public static void main(String[] args) {
     Queue<Integer> q = new LinkedList<>();
@@ -22,7 +36,8 @@ public class reverseQueue {
     q.add(20);
     q.add(30);
     q.add(40);
-    reverse(q);
+    //reverse(q);
+    reverseUpToK(q, 2);
     System.out.println(q);
   }
 }
