61f25d6927c69ce7512199fe4c71b0109f4dfbfe
satakshi003 <soumya3955@gmail.com>
2026-06-02 23:13:59 +0530

    hashmap basics

---
+++ Hash/maps/basics.java
@@ -0,0 +1,12 @@
+package Hash.maps;
+
+import java.util.HashMap;
+
+public class basics {
+  public static void main(String[] args) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    map.put(1, 1);
+    map.put(2, 3);
+    System.out.println(map);
+  } 
+}
