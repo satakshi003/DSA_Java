1b149a0ca6ffb771c4b504968841ded6bae8a11d
satakshi003 <soumya3955@gmail.com>
2026-08-02 13:55:00 +0530

    pre, in, post order

--- Binary_Trees/implementation.java
+++ Binary_Trees/implementation.java
@@ -33,6 +33,46 @@ public class implementation {
     return (root.val*product(root.left)*product(root.right));
   }
 
+  static int maxEle(Node root){
+
+    if(root == null) return Integer.MIN_VALUE;
+
+    int leftMax = maxEle(root.left);
+    int rightMax = maxEle(root.right);
+    int maxOfTwoSide = Math.max(leftMax, rightMax);
+    return Math.max(root.val, maxOfTwoSide);
+  }
+
+  static int levels(Node root){
+    if(root == null) return 0;
+
+    int rightLevel = levels(root.right);
+    int leftLevel = levels(root.left);
+
+    return 1+Math.max(rightLevel, leftLevel);
+  }
+
+   static void preOrder(Node root){
+    if(root == null) return;
+     System.out.print(root.val + " ");
+     preOrder(root.left);
+     preOrder(root.right);
+  }
+
+   static void inOrder(Node root){
+    if(root == null) return;
+     inOrder(root.left);
+     System.out.print(root.val + " ");
+     inOrder(root.right);
+  }
+
+  static void postOrder(Node root){
+    if(root == null) return;
+     postOrder(root.left);
+     postOrder(root.right);
+     System.out.print(root.val + " ");
+  }
+
   public static void main(String[] args) {
     Node a = new Node(3);
     Node b = new Node(4);
@@ -56,5 +96,15 @@ public class implementation {
     System.out.print(sum(a));
     System.out.println();
     System.out.println(product(a));
+    System.out.println();
+    System.out.print(maxEle(a));
+    System.out.println();
+    System.out.println(levels(a));
+    System.out.println();
+    preOrder(a);
+    System.out.println();
+    inOrder(a);
+    System.out.println();
+    postOrder(a);
   }
 }
